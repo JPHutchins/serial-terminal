b8efc4a2588a943a001067b959ebc25da995b6b3
J.P. Hutchins <jphutchins@gmail.com>
2023-01-11 23:09:01 -0800

    feat: handle connect/disconnect, animation, logging

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,30 @@
 use const_format::concatcp;
-use crossterm::style::Print;
-use futures::stream::StreamExt;
-use futures::{future::FutureExt, select};
-use std::format;
-use std::io::{
-    self, stdout,
-    ErrorKind::{PermissionDenied, TimedOut},
-    Write,
+use futures::{future::FutureExt, pin_mut, select, stream::StreamExt};
+use std::{
+    format,
+    io::{
+        self, stdout, Error, ErrorKind,
+        ErrorKind::{PermissionDenied, TimedOut, WouldBlock},
+        Write,
+    },
+    time::Duration,
 };
-use std::time::SystemTime;
 
+use chrono;
 use clap::{error::ContextKind::InvalidArg, error::ContextValue, Parser};
 use crossterm::{
-    cursor::{Hide, MoveLeft, Show},
+    cursor::{Hide, MoveLeft, MoveUp, Show},
     event::{Event, EventStream, KeyCode, KeyModifiers},
     execute, queue,
+    style::{
+        Color::{DarkGrey, Reset, White},
+        Print, SetBackgroundColor, SetForegroundColor,
+    },
     terminal::{disable_raw_mode, enable_raw_mode},
 };
-use tokio::io::AsyncReadExt;
-use tokio_serial::{DataBits, FlowControl, Parity, StopBits};
-
 use terminal_spinner_data::{SpinnerData, DOTS12};
+use tokio::io::AsyncReadExt;
+use tokio_serial::{DataBits, FlowControl, Parity, SerialStream, StopBits};
 
 mod arg_helpers;
 mod constants;
@@ -94,6 +98,11 @@ fn main() {
         }
     };
 
+    if args.port == "?" {
+        list_ports();
+        return;
+    }
+
     enable_raw_mode().unwrap();
 
     tokio::runtime::Builder::new_current_thread()
@@ -106,124 +115,177 @@ fn main() {
 }
 
 async fn io_tasks(args: Args) {
-    let mut serial_conn = match serial_connection::get_serial_connection(&args) {
-        Some(serial_conn) => serial_conn,
-        None => {
-            if args.port != "?" {
-                println!(
-                    "Could not open port '{}', searching for serial ports...",
-                    args.port
-                );
-            }
-            list_ports();
-            return;
-        }
-    };
-
     let mut reader = EventStream::new();
-
     let mut rx_buf: [u8; 1] = [0; 1];
 
-    let mut conn_retries = 0;
+    let connect_event_fut = wait_for_serial_port(&args, None).fuse();
+    pin_mut!(connect_event_fut);
 
-    let mut stdout = stdout();
+    'connection: loop {
+        let keypress_event = reader.next().fuse();
+        pin_mut!(keypress_event);
 
-    const ANIMATION: SpinnerData = DOTS12;
-    let mut last_animation_time = SystemTime::now();
-    let mut previous_frame_size = ANIMATION.frames[0].chars().count();
-
-    loop {
-        let mut keypress_event = reader.next().fuse();
-        let mut serial_rx_event = Box::pin(serial_conn.read_exact(&mut rx_buf).fuse());
+        let mut serial_conn: SerialStream;
 
         select! {
             event = keypress_event => {
-                match event {
-                    Some(Ok(event)) => {
-                        match handle_keypress_event(event) {
-                            KeyboardInputAction::Menu => {println!("TODO: menu"); break},
-                            KeyboardInputAction::OneByteCode(byte) => {
-                                serial_conn.write(&byte).unwrap();
+                match handle_keypress_event(&event) {
+                    KeyboardInputAction::Menu => break,
+                    KeyboardInputAction::NoAction | KeyboardInputAction::Chars(_) => continue 'connection,
+                    KeyboardInputAction::KeypressError => {log_to_ui!("Keypress error"); break}
+                };
+            },
+            event = connect_event_fut => {
+                serial_conn = event
+            },
+        }
+
+        'communication: loop {
+            let keypress_event = reader.next().fuse();
+            let serial_rx_event = serial_conn.read_exact(&mut rx_buf).fuse();
+            pin_mut!(keypress_event, serial_rx_event);
+
+            select! {
+                event = keypress_event => {
+                    match handle_keypress_event(&event) {
+                        KeyboardInputAction::Chars(bytes) => match serial_conn.write(&bytes) {
+                            Ok(_) => {},
+                            Err(error) => match error.kind() {
+                                WouldBlock => {},
+                                _ => log_to_ui!("Serial TX Error: {:?}", error)
                             }
-                            KeyboardInputAction::TwoByteCode(bytes) => {
-                                serial_conn.write(&bytes).unwrap();
+                        }
+                        KeyboardInputAction::KeypressError => {log_to_ui!("Keypress error"); break 'connection}
+                        KeyboardInputAction::Menu => break 'connection,
+                        KeyboardInputAction::NoAction => continue 'communication,
+                    };
+                },
+                event = serial_rx_event => {
+                    match event {
+                        Ok(_) => {
+                            print!("{}", rx_buf[0] as char);
+                            io::stdout().flush().unwrap();
+                        }
+                        Err(error) => {
+                            match error.kind() {
+                                PermissionDenied | TimedOut => {
+                                    connect_event_fut.set(wait_for_serial_port(&args, Some(error.kind())).fuse());
+                                    break 'communication
+                                },
+                                _ => log_to_ui!("Serial RX Error: {:?}", error)
                             }
-                            KeyboardInputAction::NoAction => continue,
                         }
                     }
-                    Some(Err(e)) => println!("Error: {:?}\r", e),
-                    None => break,
-                }
-            }
-            event = serial_rx_event => {
-                match event {
-                    Ok(_) => {
-                        print!("{}", rx_buf[0] as char);
-                        io::stdout().flush().unwrap();
-                    }
-                    Err(error) => {
-                        match error.kind() {
-                            PermissionDenied | TimedOut => {
-                                serial_conn = match serial_connection::get_serial_connection(&args) {
-                                    Some(serial_conn) => {
-                                        conn_retries = 0;
-                                        execute!(
-                                            stdout,
-                                            MoveLeft(previous_frame_size.try_into().unwrap()),
-                                            Print("  "),
-                                            Print(format!("\r\nReconnected to {}\r\n", args.port)),
-                                            Show
-                                        ).unwrap();
-                                        serial_conn
-                                    },
-                                    None => {
-                                        if conn_retries == 0 {
-                                            queue!(
-                                                stdout,
-                                                Hide,
-                                                Print(format!("Lost connection to {}  ", args.port)),
-                                                Print(ANIMATION.frames[0]),
-                                            ).unwrap();
-                                            conn_retries += 1;
-                                        } else if last_animation_time.elapsed().unwrap().as_millis() >= ANIMATION.interval.into() {
-                                            let frame = ANIMATION.frames[conn_retries % ANIMATION.frames.len()];
-                                            queue!(
-                                                stdout,
-                                                MoveLeft(previous_frame_size.try_into().unwrap()),
-                                                Print(frame),
-                                            ).unwrap();
-                                            previous_frame_size = frame.chars().count();
-                                            conn_retries += 1;
-                                            last_animation_time = SystemTime::now();
-                                        }
-                                        io::stdout().flush().unwrap();
-                                        serial_conn
-                                    }
-                                };
-                            },
-                            _ => println!("Serial RX Error: {:?}\r", error)
-                        }
-                    }
-                }
-            }
-        };
+                },
+            };
+        }
     }
 }
 
-enum KeyboardInputAction {
-    Menu,
-    NoAction,
-    OneByteCode([u8; 1]),
-    TwoByteCode([u8; 2]),
+async fn wait_for_serial_port(args: &Args, error_kind: Option<ErrorKind>) -> SerialStream {
+    const ANIMATION: SpinnerData = DOTS12;
+
+    let mut is_first_retry = true;
+    let mut stdout = stdout();
+    let mut frame_iter = ANIMATION.frames.into_iter();
+    let mut frame = frame_iter.next().unwrap();
+    let mut previous_frame_size = frame.chars().count();
+
+    loop {
+        match serial_connection::get_serial_connection(&args) {
+            Some(serial_conn) => {
+                execute!(
+                    // clear the animation and move up so log is on next line
+                    stdout,
+                    MoveLeft(previous_frame_size.try_into().unwrap()),
+                    Print("     "),
+                    MoveUp(1),
+                )
+                .unwrap();
+                log_to_ui!("Connected to {}", args.port);
+                execute!(stdout, Show).unwrap();
+                break serial_conn;
+            }
+            None => {
+                if is_first_retry {
+                    match error_kind {
+                        Some(error_kind) => {
+                            log_to_ui!("{} error '{}', waiting", args.port, error_kind)
+                        }
+                        None => log_to_ui!("Waiting for {}", args.port),
+                    };
+                    queue!(stdout, Print(frame), Hide).unwrap();
+                    is_first_retry = false;
+                } else {
+                    // let frame = ANIMATION.frames[conn_retries % ANIMATION.frames.len()];
+                    frame = match frame_iter.next() {
+                        Some(frame) => frame,
+                        None => {
+                            // restart the iterator
+                            frame_iter = ANIMATION.frames.into_iter();
+                            frame_iter.next().unwrap()
+                        }
+                    };
+                    queue!(
+                        stdout,
+                        MoveLeft(previous_frame_size.try_into().unwrap()),
+                        Print(frame),
+                    )
+                    .unwrap();
+                    previous_frame_size = frame.chars().count();
+                }
+                io::stdout().flush().unwrap();
+            }
+        };
+        tokio::time::sleep(Duration::from_millis(80)).await;
+    }
 }
 
-fn handle_keypress_event(event: Event) -> KeyboardInputAction {
+#[macro_export]
+macro_rules! log_to_ui {
+    ( $ ( $arg:tt ) * ) => {{
+        let msg = format!($($arg)*);
+        execute!(
+            stdout(),
+            Hide,
+            SetBackgroundColor(DarkGrey),
+            SetForegroundColor(White),
+            Print(format!(
+                "\r\n[{}] ",
+                chrono::offset::Local::now().format("%H:%M:%S%.3f")
+            )),
+            Print(msg),
+            SetBackgroundColor(Reset),
+            SetForegroundColor(Reset),
+            Print("\r\n"),
+            Show,
+        )
+        .unwrap();
+    }}
+}
+
+enum KeyboardInputAction {
+    Chars(Vec<u8>),
+    KeypressError,
+    Menu,
+    NoAction,
+}
+
+fn handle_keypress_event(event: &Option<Result<Event, Error>>) -> KeyboardInputAction {
+    match event {
+        Some(Ok(event)) => handle_event(&event),
+        Some(Err(_)) => KeyboardInputAction::KeypressError,
+        None => KeyboardInputAction::KeypressError,
+    }
+}
+
+fn handle_event(event: &Event) -> KeyboardInputAction {
     match event {
         Event::Key(key) => {
             if key.modifiers.contains(KeyModifiers::CONTROL) {
                 if let KeyCode::Char(code) = key.code {
                     if code == 'c' {
-                        return KeyboardInputAction::OneByteCode([0x03]);
+                        return KeyboardInputAction::Chars(vec![0x03]);
                     }
                     if code == 't' {
                         return KeyboardInputAction::Menu;
@@ -232,17 +294,17 @@ fn handle_keypress_event(event: Event) -> KeyboardInputAction {
                 return KeyboardInputAction::NoAction; // don't handle other Ctrl-* for now
             }
             match key.code {
-                KeyCode::Char(code) => KeyboardInputAction::OneByteCode([code as u8]),
-                KeyCode::Enter => KeyboardInputAction::OneByteCode([b'\r']),
-                KeyCode::Esc => KeyboardInputAction::OneByteCode([0x1B]),
-                KeyCode::Up => KeyboardInputAction::TwoByteCode([0x1B, b'A']),
-                KeyCode::Down => KeyboardInputAction::TwoByteCode([0x1B, b'B']),
-                KeyCode::Left => KeyboardInputAction::TwoByteCode([0x1B, b'C']),
-                KeyCode::Right => KeyboardInputAction::TwoByteCode([0x1B, b'D']),
-                KeyCode::Tab => KeyboardInputAction::OneByteCode([b'\t']),
-                KeyCode::Backspace => KeyboardInputAction::OneByteCode([0x08]),
-                KeyCode::Delete => KeyboardInputAction::OneByteCode([0x7F]),
-                KeyCode::Null => KeyboardInputAction::OneByteCode([0x00]),
+                KeyCode::Char(code) => KeyboardInputAction::Chars(vec![code as u8]),
+                KeyCode::Enter => KeyboardInputAction::Chars(vec![b'\r']),
+                KeyCode::Esc => KeyboardInputAction::Chars(vec![0x1B]),
+                KeyCode::Up => KeyboardInputAction::Chars(vec![0x1B, b'[', b'A']),
+                KeyCode::Down => KeyboardInputAction::Chars(vec![0x1B, b'[', b'B']),
+                KeyCode::Left => KeyboardInputAction::Chars(vec![0x1B, b'[', b'C']),
+                KeyCode::Right => KeyboardInputAction::Chars(vec![0x1B, b'[', b'D']),
+                KeyCode::Tab => KeyboardInputAction::Chars(vec![b'\t']),
+                KeyCode::Backspace => KeyboardInputAction::Chars(vec![0x08]),
+                KeyCode::Delete => KeyboardInputAction::Chars(vec![0x7F]),
+                KeyCode::Null => KeyboardInputAction::Chars(vec![0x00]),
                 _ => KeyboardInputAction::NoAction,
             }
         }
